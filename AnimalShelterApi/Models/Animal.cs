d9fab0abd4c44890ceb0bd1440968bc2eff1688d
Shaun Kent <shaunkent81@gmail.com>
2021-11-28 12:16:18 -0800

    add data-annotations to Animal class fields

--- AnimalShelterApi/Models/Animal.cs
+++ AnimalShelterApi/Models/Animal.cs
@@ -1,12 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AnimalShelterApi.Models
 {
   public class Animal
   {
     public int AnimalId { get; set; }
+    [Required]
+    [StringLength(20)]
     public string Species { get; set; }
+    [Required]
+    [StringLength(20)]
     public string Name { get; set; }
+    [Required]
+    [StringLength(20)]
     public string Gender { get; set; }
+    [Required]
+    [Range(0.1, 100, ErrorMessage = "Age must be between 0.1 and 100 years ")]
     public int Age { get; set; }
+    [Required]
     public string Backstory { get; set; }
   }
 }
