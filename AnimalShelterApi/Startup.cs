e8ff7c3d6d51a4e4ff2d17435a23fa8a574e9b08
Shaun Kent <shaunkent81@gmail.com>
2021-11-28 12:32:25 -0800

    setup SwaggerUI for API routes

--- AnimalShelterApi/Startup.cs
+++ AnimalShelterApi/Startup.cs
@@ -5,6 +5,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using AnimalShelterApi.Models;
+using System;
+using Microsoft.OpenApi.Models;
+
 namespace AnimalShelterApi
 {
     public class Startup
@@ -21,13 +24,32 @@ namespace AnimalShelterApi
             services.AddDbContext<AnimalShelterApiContext>(opt =>
             opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Animal Shelter API",
+                    Description = "An ASP.NET Core Web API for managing Animals at an animal shelter",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Shaun Kent",
+                        Url = new Uri("https://skona306.github.io/Shaun-s-Portfolio/")
+                    }
+                });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                    options.RoutePrefix = string.Empty;
+                });
             }
 
             //app.UseHttpsRedirection();
